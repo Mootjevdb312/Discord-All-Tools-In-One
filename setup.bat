19ba11b05194f128c94bb67f9695c8f827888fe2
Mootjevdb312 <84179385+Mootjevdb312@users.noreply.github.com>
2022-05-08 20:03:30 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,6 @@
 python -m pip install -r requirements.txt
 cls
 echo python astraahome.py >> "start.bat"
+$ git clone https://github.com/AstraaDev/Discord-All-Tools-In-One.git
+$ python -m pip install -r requirements.txt
+$ python3 astraahome.py
